fe8f42a89bda9a3ff6f9d3e9feadecfb1a7718fa
ADDIN Tech <subhahdzikriahmadin@gmail.com>
2025-09-03 10:49:37 +0700

    Add files via upload

---
+++ format_string.py
@@ -0,0 +1,26 @@
+# episode 18 - format string
+
+episode = "episode 18 - format string".center(100)
+print(episode)
+
+# ==========================================
+
+bulat = 16
+format_str = f"bilangan bulat = {bulat:d}"
+print(format_str)
+
+juta = 3000000
+format_str = f'3 juta = {juta:,}'
+
+desimal = 16.7
+format_str = f'des = {desimal:}'
+print(format_str)
+
+
+# hitung
+harga = 2800000
+jumlah = 1
+
+format_str = f'total hp infinix = Rp. {harga*jumlah}'
+
+print(format_str)
